3a8d6aaff89965e24e3ebfb7b1a3194591eeb9c5
LilJack118 <kamilzajaczek4@gmail.com>
2022-06-04 23:41:12 +0200

    add icon tag

--- wagtail_icons/templatetags/wagtail_icons_tags.py
+++ wagtail_icons/templatetags/wagtail_icons_tags.py
@@ -1,11 +1,32 @@
 
+import weakref
 from django import template
 from django.template.defaulttags import register
+from django.conf import settings
 import re
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
+@register.inclusion_tag('wagtail_icons/templatetags/icon.html')
+def icon(icon, size=None, class_name=None):
+    with open(icon.file.path, 'r') as f:
+        content = f.read()
+        content = str(content)
+        content = re.sub('[\t\n]+', ' ', content)
+        svg = re.search(r'<svg(.*?)>(.*)<\/svg>', content)
+        svg_parameters = svg.group(1)
+        svg_content = svg.group(2)
+        
+        if size:
+            svg_parameters = resize_icon(svg_parameters, size)
+        if class_name:
+            svg_parameters = add_new_class(svg_parameters, class_name)
 
+    return{
+        'svg_parameters':mark_safe(svg_parameters),
+        'svg_content':mark_safe(svg_content)
+    }
 
 @register.filter()
 def add_url_parameter(path, parameter):
@@ -20,3 +41,26 @@ def add_url_parameter(path, parameter):
             return f"{path}&{parameter}"
     except ValueError:
         return f"{path}?{parameter}"
+
+def resize_icon(svg_parameters, size):
+    height, width = size.split('x')
+    if not re.search(r'height="([0-9a-z ]*)"', svg_parameters):
+        svg_parameters += f' height="{str(height)}"'
+    else:
+        svg_parameters = re.sub(r'height="([0-9a-z ]*)"', f'height="{str(height)}"', svg_parameters)
+
+    if not re.search(r'width="([0-9a-z ]*)"', svg_parameters):
+        svg_parameters += f' width="{str(width)}"'
+    else:
+        svg_parameters = re.sub(r'width="([0-9a-z ]*)"', f'width="{str(width)}"', svg_parameters)
+        
+    return svg_parameters
+
+def add_new_class(svg_parameters, class_name):
+    if not re.search(r'class=".*?"', svg_parameters):
+        svg_parameters = f'class="{class_name}" {svg_parameters}'
+    else:
+        classes = re.search(r'class="(.*?)"', svg_parameters).group(1)
+        svg_parameters = re.sub(r'class=".*?"', f'class="{classes} {class_name}"', svg_parameters)
+
+    return svg_parameters
